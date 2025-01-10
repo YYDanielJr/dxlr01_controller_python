65a2c28211dbaf16b974ce0a22d46a78f2961732
Y.Y. Daniel <626986815@qq.com>
2025-01-10 22:33:23 +0800

    修复部分运行时错误

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,3 +1,11 @@
+'''
+Author: Y.Y. Daniel 626986815@qq.com
+Date: 2025-01-10 18:52:25
+LastEditors: Y.Y. Daniel 626986815@qq.com
+LastEditTime: 2025-01-10 21:40:39
+FilePath: /dxlr01_controller_python/src/data_processor.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from time import sleep
 from src.mySqlite import *
 import threading
@@ -30,23 +38,27 @@ class DataProcessor:
         while True:
             curTime = datetime.datetime.now()
             # 秒针归零自动保存短期数据
-            if curTime.second == 0 and not self.temp and not self.humi:
-                with self.listLock:
-                    self.temp.sort(reverse=True)
-                    highestTemp = self.temp[0]
-                    self.humi.sort(reverse=True)
-                    highestHumi = self.humi[0]
-                    self.temp.clear()
-                    self.humi.clear()
-                curTime.minute = curTime.minute - 1
-                curTime.microsecond = 0
-                with self.sqliteLock:
-                    self.sqliteConnector.save60minData(curTime, highestTemp, highestHumi)
-                curTime.minute = curTime.minute + 1
-                sleep(1)
+            if curTime.second == 0:
+                if len(self.humi) > 0 and len(self.temp) > 0:
+                    with self.listLock:
+                        self.temp.sort(reverse=True)
+                        highestTemp = self.temp[0]
+                        self.humi.sort(reverse=True)
+                        highestHumi = self.humi[0]
+                        self.temp.clear()
+                        self.humi.clear()
+                    # curTime.minute = curTime.minute - 1
+                    curTime = curTime - datetime.timedelta(minutes = 1)
+                    # curTime.microsecond = 0
+                    curTime = curTime.replace(microsecond=0)
+                    with self.sqliteLock:
+                        self.sqliteConnector.save60minData(curTime, highestTemp, highestHumi)
+                    # curTime.minute = curTime.minute + 1
+                    curTime = curTime + datetime.timedelta(minutes = 1)
+                    sleep(1)
 
             # 分针归0或30自动保存长期数据
-            if (curTime.minute % 5 == 0) and curTime.second == 0:
+            if (curTime.minute == 0 or curTime.minute == 30) and curTime.second == 0:
                 with self.sqliteLock:
                     datas = self.sqliteConnector.get30minData(curTime.timestamp())
 
--- src/mySqlite.py
+++ src/mySqlite.py
@@ -1,52 +1,73 @@
+'''
+Author: Y.Y. Daniel 626986815@qq.com
+Date: 2025-01-10 18:52:25
+LastEditors: Y.Y. Daniel 626986815@qq.com
+LastEditTime: 2025-01-10 21:35:29
+FilePath: /dxlr01_controller_python/src/mySqlite.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import datetime
 import sqlite3
 import time
 
 class SqliteConnector:
     def __init__(self):
-        self.conn = sqlite3.connect("./db/historyData.db")
-        self.cur = self.conn.cursor()
-        self.cur.execute('''create table if not exists last24h
+        conn = sqlite3.connect("./db/historyData.db")
+        cur = conn.cursor()
+        cur.execute('''create table if not exists last24h
                          (time real primary key not null,
                          temp real,
                          humi real);''')
-        self.cur.execute('''create table if not exists last60min
+        cur.execute('''create table if not exists last60min
                          (time real primary key not null,
                          temp real,
                          humi real);''')
-        self.conn.commit()
-
-    def __del__(self):
-        self.cur.close()
-        self.conn.close()
+        conn.commit()
+        cur.close()
+        conn.close()
 
     def save60minData(self, curDateTime: datetime, temp, humi):
-        self.cur.execute('''insert into last60min values
-                    (?, ?, ?);''', curDateTime.timestamp(), temp, humi)
-        self.conn.commit()
-        print("向last60min插入数据完成。")
+        conn = sqlite3.connect("./db/historyData.db")
+        cur = conn.cursor()
+        cur.execute('''insert into last60min values
+                    (?, ?, ?);''', (curDateTime.timestamp(), temp, humi))
+        conn.commit()
+        # print("*向last60min插入数据完成。")
 
         # 获取表中项目的数量
-        self.cur.execute('SELECT COUNT(*) FROM last60min;')
-        count = self.cur.fetchone()[0]
+        cur.execute('SELECT COUNT(*) FROM last60min;')
+        count = cur.fetchone()[0]
 
         # 如果数量超过60个，则删除最早的项目，直到数量小于等于60
         while count > 60:
-            self.cur.execute('DELETE FROM last60min WHERE time = (SELECT MIN(time) FROM last60min);')
+            cur.execute('DELETE FROM last60min WHERE time = (SELECT MIN(time) FROM last60min);')
             count -= 1
 
-        self.conn.commit()
+        conn.commit()
+        cur.close()
+        conn.close()
 
     def save24hData(self, curDateTime: datetime, temp, humi):
-        self.cur.execute('''insert into last24h values
-                    (?, ?, ?);''', curDateTime.timestamp(), temp, humi)
-        self.conn.commit()
+        conn = sqlite3.connect("./db/historyData.db")
+        cur = conn.cursor()
+        cur.execute('''insert into last24h values
+                    (?, ?, ?);''', (curDateTime.timestamp(), temp, humi))
+        conn.commit()
+        cur.close()
+        conn.close()
 
     def get60minData(self):
-        return self.cur.execute("select * from last60min;")
+        conn = sqlite3.connect("./db/historyData.db")
+        cur = conn.cursor()
+        ret = cur.execute("select * from last60min;")
+        cur.close()
+        conn.close()
+        return ret
 
     def get30minData(self, timestamp):
-        cursor = self.cur.execute("seletc * from last60min where time between ? and ?;", (timestamp - 30 * 60, timestamp))
+        conn = sqlite3.connect("./db/historyData.db")
+        cur = conn.cursor()
+        cursor = cur.execute("select * from last60min where time between ? and ?;", (timestamp - 30 * 60, timestamp))
         temp = 0.0
         humi = 0.0
         count = 0
@@ -56,4 +77,6 @@ class SqliteConnector:
             count = count + 1
         temp = temp / count
         humi = humi / count
+        cur.close()
+        conn.close()
         return [temp, humi]
